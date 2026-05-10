66f2e073e09cc3ae4ad2492c2380786caf551735
Perchik <ilya1880@mail.ru>
2026-05-10 20:05:15 +0300

    Изменил способ сохранения маршрута. route = ["","","",""] [0]-будний 1 смена [1]-будний 2 смена [2]-выходной 1 смена [3]-выходной 2 смена

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ import os
 from datetime import timedelta
 import pickle
 from pprint import pprint
-import re
 
 from sys import platform
 if platform == "win32":
     os.environ["KCFG_GRAPHICS_BORDERLESS"] = "0"
-
 
 from kivymd.app import MDApp
 from kivy.uix.popup import Popup
@@ -34,36 +32,16 @@ from kivy.properties import ListProperty, StringProperty
 from kivy.clock import Clock
 from kivy.core.window import Window
 
-# os.path.join(dir_name,FILE_STATISTIC_NAME
-
 
 WORK_TIME_FILE = "worktime_data.dat"
-ROUTE_FILE_SM_1 = "route_sm_1.dat"
-ROUTE_FILE_SM_2 = "route_sm_2.dat"
+ROUTE_FILE = "routes_data.dat"
+
+
 
 """Директория main.py"""
 dir_name = os.getcwd()
 
-def load_HDDfile_time():
-    try:
-        with open(os.path.join(dir_name,WORK_TIME_FILE), 'rb') as file:
-            file_dict = pickle.load(file)
-            print("Успешно открыт worktime_data.dat")
-            return file_dict
-    except (FileNotFoundError, IOError, EOFError):
-        # Код одноразовый для первого запуска программы
-        print("Не открылся worktime_data.dat. Создался пустой")
-        with open(os.path.join(dir_name, "worktime_data.dat"), 'wb') as obj:
-            file_dict = {}
-            pickle.dump(file_dict, obj)
-    return file_dict
-
-
-def load_HDDfile_route(sm):
-    if sm == 1:
-        open_file = ROUTE_FILE_SM_1
-    elif sm == 2:
-        open_file = ROUTE_FILE_SM_2
+def load_HDDfile(open_file):
     try:
         with open(os.path.join(dir_name,open_file), 'rb') as file:
             file_dict = pickle.load(file)
@@ -82,15 +60,27 @@ def save_HDD_DICT(dictionary:dict, name_file:str):
         pickle.dump(dictionary, file)
 
 
-DICT_TIME_STATISTIC = load_HDDfile_time()
-# DICT_TIME_STATISTIC = {}
-DICT_ROUT_1 = load_HDDfile_route(1)
-# DICT_ROUT = {}
-DICT_ROUT_2 = load_HDDfile_route(2)
-pprint(DICT_ROUT_1)
-print("#########################################")
-pprint(DICT_ROUT_2)
+
+DICT_TIME = load_HDDfile(WORK_TIME_FILE)
+DICT_ROUTE = load_HDDfile(ROUTE_FILE)
+
+
+
+print("DICT_TIME_STATISTIC############")
+pprint(DICT_TIME)
 print("+++++++++++++++++++++++++++++++++")
+print("DICT_ROUTE ####################")
+pprint(DICT_ROUTE)
+print("+++++++++++++++++++++++++++++++++")
+
+
+
+
+
+
+
+
+
 month_lst = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
              'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
 
@@ -104,12 +94,6 @@ else:
 
 number_month = int(time.strftime("%m", time_day))
 CURRENT_MONTH = month_lst[number_month - 1]
-
-
-
-
-
-
 
 class PagesManager(MDScreenManager):
     def __init__(self, **kwargs):
@@ -141,11 +125,9 @@ class PagesManager(MDScreenManager):
     #             self.current = "setting_page"
     #     return super(PagesManager, self).on_touch_up(touch)
 
-
-
 def checking_the_time(hours,minutes):
     if hours.isdigit() and minutes.isdigit():
-        return hours + ":" + minutes
+        return hours.zfill(2) + ":" + minutes.zfill(2)
     else:
         return "00 : 00"
 
@@ -176,12 +158,13 @@ def check_value_is_numeric(value:(tuple,list))->bool:
     except (ValueError,TypeError):
         return False
 
-def check_smena(start_time):
-    if int(start_time) < 12:
-        return DICT_ROUT_1,ROUTE_FILE_SM_1
-    else:
-        return DICT_ROUT_2,ROUTE_FILE_SM_2
-
+def get_route(route_str:str):
+    try:
+        route,karta = route_str.split("/")
+        return route,karta
+    except ValueError:
+        route, karta = "Не введён", ""
+        return route, karta
 
 ################################################################################
 ################################################################################
@@ -204,6 +187,30 @@ class Page_main(MDScreen):
     def __init__(self, **kwargs):
         MDScreen.__init__(self, **kwargs)
 
+    def show_DICT(self):
+        print("DICT_TIME #########################################")
+        pprint(DICT_TIME)
+
+        print("DICT_ROUTE_WKDAY #########################################")
+        pprint(DICT_ROUTE)
+        print("+++++++++++++++++++++++++++++++++")
+
+
+    def check_route_in_dict(self, route) -> list:
+        try:
+            time_in_route = DICT_ROUTE[route]
+            return time_in_route
+        except KeyError:
+            return ["", "", "", ""]
+
+
+    def get_button_smena(self):
+        if self.ids.sm_1.state == "down":
+            return 0
+        elif self.ids.sm_2.state == "down":
+            return 1
+
+
     def get_user_choice_date(self):
         day = self.ids["spinner_day"].text
         month = self.ids["spinner_month"].text
@@ -211,8 +218,8 @@ class Page_main(MDScreen):
 
     def search_rout_in_dict(self):
         route_and_karta = self.get_route_user_choice()
-        if route_and_karta in DICT_ROUT_1:
-            list_time = DICT_ROUT_1[route_and_karta]
+        if route_and_karta in DICT_ROUTE:
+            list_time = DICT_ROUTE[route_and_karta]
             HW_start = list_time[0]
             MW_start = list_time[1]
             HW_end = list_time[2]
@@ -222,6 +229,8 @@ class Page_main(MDScreen):
             HL_end = list_time[6]
             ML_end = list_time[7]
             self.install_time_in_textinput(HW_start, MW_start, HW_end, MW_end, HL_start, ML_start, HL_end, ML_end)
+        self.calculation_of_working()
+
 
     def install_time_in_textinput(self, HW_start, MW_start, HW_end, MW_end, HL_start, ML_start, HL_end, ML_end):
 
@@ -276,51 +285,79 @@ class Page_main(MDScreen):
         self.total_hours_work = tot_time[0]
         self.total_minutes_work = tot_time[1]
 
+    def check_route_input(self):
+        if self.get_route_user_choice() != "Не введён":
+            self.ids.create_route.disabled = False
+        else:
+            self.ids.create_route.disabled = True
+
+
     def calculation_of_working(self):
         all_time_user_input:list = self.get_all_time_user_input() # возвр все часы\минуты
         if check_value_is_numeric(all_time_user_input):
             total_time = calculate_work_time(all_time_user_input)
-
             self.install_total_working_time(total_time)
             self.ids.save_button.disabled = False
+            self.check_route_input()
         else:
             self.ids.save_button.disabled = True
+            self.ids.create_route.disabled = True
             self.total_hours_work = "0"
             self.total_minutes_work = "00"
 
-
-############################################################################################################
-    def intercept_data_main_screen(self):
-        date_choice_user: str = self.get_user_choice_date() # возвр дату => "23 Май"
+    def intercept_and_save_alldata(self):
+        KEY: str = self.get_user_choice_date() # возвр дату => "23 Май"
         route_and_karta_in_day: str = self.get_route_user_choice() # возвр маршрут => 102/4 или "Не введён"
-        all_time_user_input = self.get_all_time_user_input() # возвр все часы\минуты
-
-
-
-        # self.save_date_in_dict_time(date_choice_user, route_and_karta_in_day, total_time)
-
-
-
-
-
-
-    def save_date_in_dict_time(self, key: str, route: str, tot_time: tuple[str, str]):
-        label_text_save = self.ids["savingtext"]
-        route_and_time: list = [route,tot_time]
-        check_key = self.check_day_in_dict(key,flag="date")
-        if check_key:
-            print("Popup")
-            MyPopup_save_worktime(check_key,route_and_time,label_text_save)
-        else:
-            print("else")
-            DICT_TIME_STATISTIC[key] = route_and_time
-            save_HDD_DICT_TIME(DICT_TIME_STATISTIC, "worktime_data.dat")
+        all_time_user_input:list = self.get_all_time_user_input() # возвр все часы\минуты
+        label_save_text = self.ids.savingtext
+        if KEY not in DICT_TIME:
+            DICT_TIME[KEY] = (route_and_karta_in_day,
+                              (self.total_hours_work, self.total_minutes_work),
+                              all_time_user_input)
+            save_HDD_DICT(DICT_TIME, WORK_TIME_FILE)
             self.change_save_text_label()
+        else:
+            MyPopup_save_new_worktime(KEY,route_and_karta_in_day,(self.total_hours_work, self.total_minutes_work),
+                                  all_time_user_input,label_save_text)
+        pprint(DICT_TIME,)
+        print("строка 322")
+
+
+
+    def create_route_kart(self):
+        num_route:str = self.get_route_user_choice() # Получ.маршрут 102/4
+        all_time_route:list = self.get_all_time_user_input() # Получ.время маршрута
+        button_smena = self.get_button_smena()
+        list_time_in_route:list = self.check_route_in_dict(num_route)
+        label_savetext = self.ids.savingtext
+        """Popup Вопрос: карта выходного или буднего дня"""
+        MyPopup_weekday_or_weekend(num_route,all_time_route,button_smena,list_time_in_route,label_savetext)
+
+
+        # DICT,FILE = check_smena(all_time_route[0])
+        # DICT[route] = all_time_route
+        # self.change_save_text_label()
+        # save_HDD_DICT(DICT,FILE)
+
+
+    def my_callback(self, instance):
+        self.ids.savingtext.text_color = "black"
+        self.ids.savingtext.text = "Отработано:"
+        return False
+
+    def change_save_text_label(self):
+        Clock.schedule_once(self.my_callback, 2)
+        # self.label.theme_text_color = "Custom"
+        self.ids.savingtext.text_color = "red"
+        self.label.text = "Сохранено"
 
 
 
 
 
+
+
+#######################################################################################
 
     def save_data_in_dict_route(self, route: str, spinners: list):
         label_text_save = self.ids["savingtext"]
@@ -328,14 +365,9 @@ class Page_main(MDScreen):
         if check_key:
             MyPoput_save_route(route, spinners, label_text_save)
         else:
-            DICT_ROUT_1[route] = spinners
-            save_HDD_DICT_TIME(DICT_ROUT_1, "route_data.dat")
+            DICT_ROUTE[route] = spinners
+            save_HDD_DICT_TIME(DICT_ROUTE, "route_data.dat")
             self.change_save_text_label()
-
-
-
-
-
 
     def my_callback(self, instance):
         self.ids["savingtext"].text_color = "black"
@@ -351,27 +383,45 @@ class Page_main(MDScreen):
 
 
 
-class MyPopup_save_worktime(Popup):
-    message_info = StringProperty("Рабочий день на эту дату существует.\n Переписать?")
+class MyPopup_save_new_worktime(Popup):
+
+    curr_data = StringProperty()
+    route_str = StringProperty()
+    karta_str = StringProperty()
+    start_work = StringProperty()
+    end_work = StringProperty()
+    start_lunch = StringProperty()
+    end_lunch = StringProperty()
+
     Builder.load_file(os.path.join(dir_name, "Popup_new_time.kv"))
-    def __init__(self,date,work_time,lab,**kwargs):
+    def __init__(self,date,route,tot_work_time,lst_time, lab,**kwargs):
         Popup.__init__(self, **kwargs)
-        self.key = date
-        self.work_time = work_time
+        self.KEY = date
+        self.tot_work_time = tot_work_time
+        self.route = route
+
+        self.list_time = lst_time
+        self.install_time_work_in_labels()
         self.label = lab
+        self.curr_data = date
         self.open()
 
+    def install_time_work_in_labels(self):
+        route_and_karta = DICT_TIME[self.KEY][0]
+        self.route_str, self.karta_str = get_route(route_and_karta)
+        list_time = DICT_TIME[self.KEY][2]
+        self.start_work = checking_the_time(list_time[0],list_time[1])
+        self.end_work = checking_the_time(list_time[2],list_time[3])
+        self.start_lunch = checking_the_time(list_time[4],list_time[5])
+        self.end_lunch = checking_the_time(list_time[6],list_time[7])
+
     def answer_ok(self):
-        print("worktime_data.dat")
-        DICT_TIME_STATISTIC[self.key] = self.work_time
-        save_HDD_DICT_TIME(DICT_TIME_STATISTIC, "worktime_data.dat")
+        print("answer_ok")
+        DICT_TIME[self.KEY] = (self.route, self.tot_work_time, self.list_time)
+        save_HDD_DICT(DICT_TIME, WORK_TIME_FILE)
         self.change_save_text_label()
-        # else:
-        #     DICT_ROUT[self.key] = self.work_time
-        #     save_HDD_DICT_TIME(DICT_ROUT, "route_data.dat")
-        #     self.change_save_text_label()
         self.dismiss()
-        return
+        pprint(DICT_TIME)
 
     def my_callback(self, instance):
         self.label.text_color = "black"
@@ -380,11 +430,55 @@ class MyPopup_save_worktime(Popup):
 
     def change_save_text_label(self):
         Clock.schedule_once(self.my_callback, 2)
-        self.label.theme_text_color = "Custom"
         self.label.text_color = "red"
         self.label.text = "Сохранено"
 
-class MyPopup_save_route(Popup):
+class MyPopup_weekday_or_weekend(Popup):
+    Builder.load_file(os.path.join(dir_name, "Popup_wkd_or_wknd.kv"))
+    def __init__(self, num_route, all_time_new, smena, all_time_route:list["","","",""],lab, **kwargs):
+        Popup.__init__(self, **kwargs)
+        self.num_route = num_route
+        self.all_time_new = all_time_new
+        self.smena = smena
+        self.all_time_route:list = all_time_route
+        self.label_savetext = lab
+        self.open()
+
+    def weekday(self):
+        if not self.all_time_route[self.smena]:
+            self.all_time_route[self.smena] = self.all_time_new
+            DICT_ROUTE[self.num_route] = self.all_time_route
+            save_HDD_DICT(DICT_ROUTE, ROUTE_FILE)
+            self.change_save_text_label()
+        else:
+            MyPopup_save_new_route(self.num_route, self.all_time_new, self.smena,self.label_savetext)
+
+
+    def weekend(self):
+        """Выходной день- индекс для списка маршрута self.smena + 2"""
+        self.smena += 2
+        if not self.all_time_route[self.smena]:
+            self.all_time_route[self.smena] = self.all_time_new
+            DICT_ROUTE[self.num_route] = self.all_time_route
+            save_HDD_DICT(DICT_ROUTE, ROUTE_FILE)
+            self.change_save_text_label()
+        else:
+            MyPopup_save_new_route(self.num_route, self.all_time_new, self.smena, self.label_savetext)
+
+
+    def my_callback(self, instance):
+        self.label_savetext.text_color = "black"
+        self.label_savetext.text = "Отработано:"
+        return False
+
+    def change_save_text_label(self):
+        Clock.schedule_once(self.my_callback, 3)
+        self.label_savetext.text_color = "red"
+        self.label_savetext.text = "Сохранено"
+
+
+
+class MyPopup_save_new_route(Popup):
     route_str = StringProperty()
     karta_str = StringProperty()
 
@@ -394,43 +488,39 @@ class MyPopup_save_route(Popup):
     end_lunch = StringProperty()
 
     Builder.load_file(os.path.join(dir_name, "Popup_new_route.kv"))
-    def __init__(self,dct,key,file_name,new_array_time, **kwargs):
+    def __init__(self, route_key, new_array_time, smena:int,lab, **kwargs):
         Popup.__init__(self, **kwargs)
-        self.dct = dct
-        self.key = key
-        self.file_name = file_name
+        self.key = route_key
+        self.route_str, self.karta_str = route_key.split("/")
         self.new_array_time = new_array_time
-        self.route_str,self.karta_str = key.split("/")
-        self.box_word_save = MDBoxLayout(WordSave_or_Time(text="Сохранено",
-                                                          text_color="red",
-                                                          role="large"))
+        self.smena:int = smena
         self.pars_dict_old_route()
+        self.label_savetext = lab
+        self.open()
 
     def pars_dict_old_route(self):
         """Ф-ция устанавливает время старого маршрута в Popup для
         ознакомления пользователя, что он меняет."""
-        self.start_work = checking_the_time(self.dct[self.key][0],self.dct[self.key][1])
-        self.end_work = checking_the_time(self.dct[self.key][2],self.dct[self.key][3])
-        self.start_lunch = checking_the_time(self.dct[self.key][4],self.dct[self.key][5])
-        self.end_lunch = checking_the_time(self.dct[self.key][6],self.dct[self.key][7])
-
-
-
+        self.start_work = checking_the_time(DICT_ROUTE[self.key][self.smena][0],DICT_ROUTE[self.key][self.smena][1])
+        self.end_work = checking_the_time(DICT_ROUTE[self.key][self.smena][2],DICT_ROUTE[self.key][self.smena][3])
+        self.start_lunch = checking_the_time(DICT_ROUTE[self.key][self.smena][4],DICT_ROUTE[self.key][self.smena][5])
+        self.end_lunch = checking_the_time(DICT_ROUTE[self.key][self.smena][6],DICT_ROUTE[self.key][self.smena][7])
 
     def answer_ok(self):
-        self.dct[self.key] = self.new_array_time
-        save_HDD_DICT(self.dct, self.file_name)
-        self.change_word_save()
-
-    def my_callback(self,qt):
+        DICT_ROUTE[self.key][self.smena] = self.new_array_time
+        save_HDD_DICT(DICT_ROUTE, ROUTE_FILE)
+        self.change_save_text_label()
         self.dismiss()
 
-    def change_word_save(self):
-        """После сохранения, вместо кнопок, появляется
-        красная надпись 'Сохранено'. """
-        Clock.schedule_once(self.my_callback, 1)
-        self.box_answer_ok.clear_widgets()
-        self.box_answer_ok.add_widget(self.box_word_save)
+    def my_callback(self, instance):
+        self.label_savetext.text_color = "black"
+        self.label_savetext.text = "Отработано:"
+        return False
+
+    def change_save_text_label(self):
+        Clock.schedule_once(self.my_callback, 3)
+        self.label_savetext.text_color = "red"
+        self.label_savetext.text = "Сохранено"
 
 class Page_stat(MDScreen):
     Builder.load_file(os.path.join(dir_name,"statistic_page.kv"))
@@ -443,6 +533,7 @@ class RouteTextInput(MDTextField):
         MDTextField.__init__(self, **kwargs)
         self.halign = "center"
         self.bold = True
+        self.theme_text_color = "Custom"
     def insert_text(self, value, from_undo=False):
         if value.isdigit():
             if len(self.text) < 3:
@@ -463,6 +554,8 @@ class KartaTextInput(MDTextField):
         MDTextField.__init__(self, **kwargs)
         self.halign = "center"
         self.bold = True
+        self.theme_text_color = "Custom"
+
     def insert_text(self, value, from_undo=False):
         if value.isdigit():
             if len(self.text) < 2:
